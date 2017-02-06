01133c94aa7a008b459cc16cca5b88d146f1fd5e
Tim Lu <victor.primoris@gmail.com>
2017-02-06 14:11:13 -0800

    Modified home.php to include partials

--- home.php
+++ home.php
@@ -1,2 +1,6 @@
 <?php get_header(); ?>
+
+<?php echo get_template_part('partials/home-landing.php', 'landing'); ?>
+<?php echo get_template_part('partials/home-packages.php', 'packages'); ?>
+
 <?php get_footer(); ?>
